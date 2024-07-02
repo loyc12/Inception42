c5f1f0991c1611e5e0dee12c307cdabdea2750c1
Loyc-Lubuntu-Inception <lordloyc@gmail.com>
2024-07-02 16:50:17 -0400

    sdgkhsdkjlghdsklfjg

--- srcs/requirements/mariadb/init.sh
+++ srcs/requirements/mariadb/init.sh
@@ -3,31 +3,26 @@
 # Checks if the database exists
 if [ -d "/var/lib/mysql/${MYSQL_DTBS_NAME}" ]
 then
-	echo "Database already exists"
+	echo "Database already exists : Skipping creation"
 
 # If it doesn't, initiates it
 else
-
 chown -R mysql:"${MYSQL_HOST_NAME}" /var/lib/mysql
 mysql_install_db --datadir=/var/lib/mysql --user="${MYSQL_HOST_NAME}" --skip-test-db >> /dev/null
 
-# Initiates the database via the mysql cli ( TODO : add --silent ) ( --bootstrap no worky ? )
-mysql --user="${MYSQL_HOST_NAME}" << _EOF_
+touch tmpfile.sql.fuckthatshit
+chmod 777 tmpfile.sql.fuckthatshit
 
-FLUSH PRIVILEGES;
-CREATE DATABASE IF NOT EXISTS "${MYSQL_DTBS_NAME}";
+echo "FLUSH PRIVILEGES;" > tmpfile.sql.fuckthatshit
+echo "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DTBS_NAME}\`;" > tmpfile.sql.fuckthatshit
 
-CREATE USER IF NOT EXISTS "${MYSQL_USER_NAME}"@'%' IDENTIFIED BY "${MYSQL_USER_PSWD}";
-GRANT ALL PRIVILEGES ON "${MYSQL_DTBS_NAME}"'.*' TO "${MYSQL_USER_NAME}"@'%' WITH GRANT OPTION;
+echo "CREATE USER IF NOT EXISTS \`${MYSQL_USER_NAME}\`@'%' IDENTIFIED BY '${MYSQL_USER_PSWD}';" > tmpfile.sql.fuckthatshit
+echo "GRANT ALL PRIVILEGES ON \`${MYSQL_DTBS_NAME}\`'.*' TO \`${MYSQL_USER_NAME}\`@'%' WITH GRANT OPTION;" > tmpfile.sql.fuckthatshit
 
-ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PSWD}'
-GRANT ALL PRIVILEGES ON '*.*' TO 'root'@'localhost' WITH GRANT OPTION;";
+echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PSWD}';" > tmpfile.sql.fuckthatshit
+echo "GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' WITH GRANT OPTION;" > tmpfile.sql.fuckthatshit
 
-FLUSH PRIVILEGES;
-
-_EOF_
-
-# IDENTIFIED BY "${MYSQL_USER_PSWD}"
+echo "FLUSH PRIVILEGES;" > tmpfile.sql.fuckthatshit
 
 # What the above script does ( in order ) :
 
@@ -35,6 +30,27 @@ _EOF_
 # Creates the initial database, since it doesn't exist yet
 # Adds a root user on 127.0.0.1 to allow remote connexion
 # Adds an admin user on 127.0.0.1
+
+# Initiates the database via the mysql cli ( TODO : add --silent )
+# Does not work with a heredoc because of mysql's cli shitty syntax requirements, afaik
+mysqld --user="${MYSQL_HOST_NAME}" --bootstrap < tmpfile.sql.fuckthatshit
+
+rm -rf tmpfile.sql.fuckthatshit
+
+#mysqld --user="${MYSQL_HOST_NAME}" --bootstrap << _EOF_
+#
+#FLUSH PRIVILEGES
+#CREATE DATABASE IF NOT EXISTS ${MYSQL_DTBS_NAME}
+#
+#CREATE USER IF NOT EXISTS ${MYSQL_USER_NAME}@'%' IDENTIFIED BY '${MYSQL_USER_PSWD}'
+#GRANT ALL PRIVILEGES ON ${MYSQL_DTBS_NAME}'.*' TO ${MYSQL_USER_NAME}@'%' WITH GRANT OPTION
+#
+#ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PSWD}'
+#GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' WITH GRANT OPTION
+#
+#FLUSH PRIVILEGES
+#
+#_EOF_
 
 fi
 
--- srcs/requirements/wordpress/init.sh
+++ srcs/requirements/wordpress/init.sh
@@ -3,27 +3,28 @@
 # Creates the needed directory
 if [ ! -d /run/php ]
 then
-
 	service php7.3-fpm start
 	service php7.3-fpm stop
-
 fi
 
-# Checks if wp-config.php exists, else downloads wordpress
-if [ -f /var/www/html/wp-config.php ]
+# Checks if wp-config.php exists, and if so, skips the installation
+if [ ! -f /var/www/html/wp-config.php ]
 then
-	echo "Wordpress already downloaded; skipping installation"
-else
-
 	# downloads wordpress and its config file
 	wp core download --allow-root --path=/var/www/html
-	wp config create --allow-root --dbname="${MYSQL_DTBS_NAME}" --dbuser="${MYSQL_USER_NAME}" --dbpass="${MYSQL_PSWD}" --dbhost=mariadb:3306
-	wp core install --allow-root --url="${DOMAIN_NAME}" --title="${WP_TITLE}" --admin_name="${WP_ADMIN_NAME}" --admin_password="${WP_ADMIN_PSWD}" --skip-email
-	wp user create --allow-root "${WP_USER_NAME}" "${WP_USER_EMAIL}" --user_pass="${WP_USER_PSWD}" --role=author
+	wp config create --allow-root --dbname=${MYSQL_DTBS_NAME} --dbuser=${MYSQL_USER_NAME} --dbpass=${MYSQL_ROOT_PSWD} --dbhost=${WP_DTBS_HOST}
+	wp core install --allow-root --url=${DOMAIN_NAME} --title=${WP_TITLE} --admin_name=${WP_ADMIN_NAME} --admin_password=${WP_ADMIN_PSWD} --admin_email=${WP_ADMIN_EMAIL} --skip-email
+	wp user create --allow-root ${WP_USER_NAME} ${WP_USER_EMAIL} --user_pass=${WP_USER_PSWD} --role=author
+	#/usr/sbin/php-fpm7.3 -F
+else
+	echo "Wordpress already downloaded ; skipping installation"
 fi
 
 # Launches PHP-FPM in the forground and not as a daemon
 # This means that the process will not be detached from the terminal
 /usr/sbin/php-fpm7.3 -F
 
-f
+#if [ ! -f /var/www/html/wp-config.php ]
+#then
+#	/usr/sbin/php-fpm7.3 -F
+#fi
