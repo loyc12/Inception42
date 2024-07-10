72a28ca2d1c8bf5417de1ad6578326ed86475cd0
Loyc-Lubuntu-Inception <lordloyc@gmail.com>
2024-07-10 14:48:22 -0400

    finishing touches

--- srcs/requirements/mariadb/init.sh
+++ srcs/requirements/mariadb/init.sh
@@ -4,35 +4,25 @@
 chown -R mysql:mysql /var/lib/mysql
 mysql_install_db --datadir=/var/lib/mysql --user=mysql --skip-test-db >> /dev/null
 
-touch tmpfile.sql.fts
-chmod 777 tmpfile.sql.fts
+mysqld --user=mysql --bootstrap << _EOF_
+CREATE DATABASE IF NOT EXISTS \`${MYSQL_DTBS_NAME}\`;
 
-# Allows ( via FLUSH ) the sql tables to be updated automatically when they are modified
-echo "FLUSH PRIVILEGES;" > tmpfile.sql.fts
-# Creates the initial database, since it doesn't exist yet
-echo "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DTBS_NAME}\`;" >> tmpfile.sql.fts
+CREATE USER IF NOT EXISTS \`${MYSQL_USER_NAME}\`@'%' IDENTIFIED BY '${MYSQL_USER_PSWD}';
+GRANT ALL PRIVILEGES ON \`${MYSQL_DTBS_NAME}\`.* TO \`${MYSQL_USER_NAME}\`@'%';
 
-# Adds an admin user on 127.0.0.1
-echo "CREATE USER IF NOT EXISTS \`${MYSQL_USER_NAME}\`@'%' IDENTIFIED BY '${MYSQL_USER_PSWD}';" >> tmpfile.sql.fts
-echo "GRANT ALL PRIVILEGES ON \`${MYSQL_DTBS_NAME}\`.* TO \`${MYSQL_USER_NAME}\`@'%';" >> tmpfile.sql.fts
+ALTER USER \`root\`@\`localhost\` IDENTIFIED BY '${MYSQL_ROOT_PSWD}';
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' WITH GRANT OPTION;
 
-# Adds a root user on 127.0.0.1 to allow remote connexion
-echo "ALTER USER \`root\`@\`localhost\` IDENTIFIED BY '${MYSQL_ROOT_PSWD}';" >> tmpfile.sql.fts
-echo "GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' WITH GRANT OPTION;" >> tmpfile.sql.fts
+FLUSH PRIVILEGES;
+_EOF_
 
-echo "FLUSH PRIVILEGES;" >> tmpfile.sql.fts
+# PS : These two kinds of apostrophes are needed to distinguish between string literals ( '_' ) and sql identifiers ( `_` )
 
-# The above script needs the two types of "'", because reasons
-
-# Initiates the database via the above script
-mysqld --user=mysql --bootstrap < tmpfile.sql.fts
-
-rm -rf tmpfile.sql.fts
+# The above script, in order, does the following :
+# - Creates the initial database
+# - Adds an admin user on it
+# - Adds a root user on
+# - Forces the database to update its tables
 
 echo "Database initiated"
-
 exec mysqld_safe
-
-echo "Database launched"
-
-# WIP
